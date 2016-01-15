36519da05280a2ac907756a0a3f7993dfb68002a
Daniel Suo <danielsuo@gmail.com>
2016-01-15 15:59:14 -0500

    Add Residuals.h

---
+++ Residuals.h
@@ -0,0 +1,13 @@
+#ifndef RESIDUALS_H
+#define RESIDUALS_H
+
+#include "BundleAdjustmentResidual.h"
+#include "PoseGraphResidual.h"
+#include "AlignmentTriangulateResidual.h"
+#include "Alignment2DResidual.h"
+#include "Alignment3DResidual.h"
+#include "AlignmentBoxResidual.h"
+#include "Alignment2D3DResidual.h"
+#include "Alignment2DFocalLengthResidual.h"
+
+#endif
