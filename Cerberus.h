d470e4a4523448ccd3b02116bf05351987bdc1cd
Daniel Suo <danielsuo@gmail.com>
2016-01-19 00:12:07 -0500

    Scratch that

--- Cerberus.h
+++ Cerberus.h
@@ -30,35 +30,27 @@ template <typename T, typename U> void TransformationMatrixToAngleAxisAndTransla
     tmp[i] = U(Rt[i]);
   }
 
-  TransformationMatrixToAngleAxisAndTranslation(tmp, result);
-}
-
-template <typename T> void TransformationMatrixToAngleAxisAndTranslation(T *Rt, T *result) {
-  RotationMatrixToAngleAxis<T>(MatrixAdapter<const T, 4, 1>(Rt), result);
-  result[3] = Rt[3];
-  result[4] = Rt[7];
-  result[5] = Rt[11];
+  RotationMatrixToAngleAxis<U>(MatrixAdapter<const U, 4, 1>(tmp), result);
+  result[3] = tmp[3];
+  result[4] = tmp[7];
+  result[5] = tmp[11];
 }
 
 // At is a 6-dimensional vector with first three terms representing axis,
 // magnitude angle, and the last three terms represent translation. Rt is a
-// 4x3 transformation matrix of the form [R|t]. Two typenames because Ceres
-// requires doubles.
+// 4x3 transformation matrix of the form [R|t]. Two typenames
+// because Ceres requires doubles.
 template <typename T, typename U> void AngleAxisAndTranslationToTransformationMatrix(const U *At, T *result) {
   U tmp[12];
 
-  AngleAxisAndTranslationToTransformationMatrix(At, tmp);
+  AngleAxisToRotationMatrix<U>(At, MatrixAdapter<U, 4, 1>(tmp));
+  tmp[3] = At[3];
+  tmp[7] = At[4];
+  tmp[11] = At[5];
 
   for (int i = 0; i < 12; i++) {
     result[i] = T(tmp[i]);
   }
-}
-
-template <typename T> void AngleAxisAndTranslationToTransformationMatrix(const T *At, T *result) {
-  AngleAxisToRotationMatrix<T>(At, MatrixAdapter<T, 4, 1>(result));
-  result[3] = At[3];
-  result[7] = At[4];
-  result[11] = At[5];
 }
 
 #endif
