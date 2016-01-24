04c60a06e9bfd787d3d2221523bd461ef666d8c2
Fisher Yu <i@yf.io>
2016-01-24 17:24:05 -0500

    add relative rotation cost function

--- Residuals/relative_pose.h
+++ Residuals/relative_pose.h
@@ -11,7 +11,7 @@ class RelativeDistanceCostFunctor {
  public:
   RelativeDistanceCostFunctor(const Eigen::Vector3d &position0,
                               const Eigen::Vector3d &position1,
-                              double weight = 0) {
+                              double weight = 1) {
     distance_ = (position0 - position1).norm();
     weight_ = weight;
   }
@@ -22,12 +22,64 @@ class RelativeDistanceCostFunctor {
     for (int i = 0; i < 3; ++i) {
       residuals[0] += (p0[i] - p1[i]) * (p0[i] - p1[i]);
     }
-    residuals[0] = ceres::sqrt(residuals[0]);
+    residuals[0] = weight_ * (ceres::sqrt(residuals[0]) - T(distance_));
     return true;
   }
 
  private:
   double distance_;
+  double weight_;
+};
+
+// Square matrix multiplication. The first matrix is transposed.
+template <int Size, typename T>
+void mtxm(const T* const m0, const T* const m1, T* p) {
+  for (int r = 0; r < Size; ++r) {
+    for (int c = 0; c < Size; ++c) {
+      *p = T(0);
+      for (int i = 0; i < Size; ++i) {
+        *p += m0[i * Size + r] * m1[i * Size + c];
+      }
+      ++p;
+    }
+  }
+}
+
+// Calculate angular distance between relative rotation
+class RelativeRotationCostFunctor {
+ public:
+  RelativeRotationCostFunctor(const Eigen::Vector3d &angle_axis_0,
+                              const Eigen::Vector3d &angle_axis_1,
+                              double weight = 1) {
+    relative_rotation_.resize(9);
+    std::vector<double> r0(9), r1(9);
+    ceres::AngleAxisToRotationMatrix(angle_axis_0.data(), r0.data());
+    ceres::AngleAxisToRotationMatrix(angle_axis_1.data(), r1.data());
+    mtxm<3>(r0.data(), r1.data(), relative_rotation_.data());
+    weight_ = weight;
+  }
+
+  template <typename T>
+  bool operator () (const T* const aa0, const T* const aa1,
+                    T* residuals) const {
+    T r0[9], r1[9], r0tr1[9], rtr[9];
+    for (int i = 0; i < 9; ++i) {
+      rtr[i] = T(relative_rotation_[i]);
+    }
+    ceres::AngleAxisToRotationMatrix(aa0, r0);
+    ceres::AngleAxisToRotationMatrix(aa1, r1);
+    mtxm<3>(r0, r1, r0tr1);
+    // Reuse memory
+    mtxm<3>(rtr, r0tr1, r0);
+    ceres::RotationMatrixToAngleAxis(r0, r1);
+    residuals[0] = T(weight_) *
+        ceres::sqrt(r1[0] * r1[0] + r1[1] * r1[1] + r1[2] * r1[2]);
+
+    return true;
+  }
+
+ private:
+  std::vector<double> relative_rotation_;
   double weight_;
 };
 
